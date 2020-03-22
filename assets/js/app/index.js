a98ee760b9023fe95db620fe95d5216ec9c0c1c2
Josh Perry <josh@6bit.com>
2020-03-22 12:32:13 -0600

    Refactor scroll-based logic to use Waypoints.js

--- assets/js/app/index.js
+++ assets/js/app/index.js
@@ -6,52 +6,12 @@ require(['app/lists-common'])
 /**
  * Index page logic
  */
-require([], () => {
-  // TODO: Update to use waypoints.js
+require(['waypoints'], (Waypoint) => {
   // Show top nav when hero scrolls out of view
-  const nav = document.querySelector('.site-nav-main .site-nav')
-  const feed = document.querySelector('.post-feed')
-
-  let lastScrollY = window.scrollY
-  let lastWindowHeight = window.innerHeight
-
-  let lastDocumentHeight = document.documentElement.scrollHeight
-  let ticking = false
-
-  function onScroll() {
-    lastScrollY = window.scrollY
-    requestTick()
-  }
-
-  function onResize() {
-    lastWindowHeight = window.innerHeight
-    lastDocumentHeight = document.documentElement.scrollHeight
-    requestTick()
-  }
-
-  function requestTick() {
-    if (!ticking) {
-      requestAnimationFrame(update)
-    }
-    ticking = true
-  }
-
-  function update() {
-    const trigger = feed.getBoundingClientRect().top + window.scrollY
-    const progressMax = lastDocumentHeight - lastWindowHeight
-
-    // show/hide nav
-    if (lastScrollY >= trigger - 20) {
-      nav.classList.add('fixed-nav-active')
-    } else {
-      nav.classList.remove('fixed-nav-active')
-    }
-
-    ticking = false
-  }
-
-  window.addEventListener('scroll', onScroll, { passive: true })
-  window.addEventListener('resize', onResize, false)
-
-  update()
+	new Waypoint({
+		element: document.querySelector('.post-feed'),
+		handler: dir => document
+											.querySelector('.site-nav-main .site-nav')
+											.classList.toggle('fixed-nav-active', dir === 'down')
+	})
 })
--- assets/js/app/post.js
+++ assets/js/app/post.js
@@ -6,12 +6,13 @@ require(['app/content-common'])
 /**
  * Post specific logic
  */
-require([], () => {
+require(['waypoints'], (Waypoint) => {
   // Replace nav with title on scroll
-  stickyNavTitle({
-    navSelector: '.site-nav-main',
-    titleSelector: '.post-full-title',
-    activeClass: 'nav-post-title-active'
+  new Waypoint({
+    element: document.querySelector('.post-full-title'),
+    handler: dir => document
+                      .querySelector('.site-nav-main')
+                      .classList.toggle('nav-post-title-active', dir === 'down')
   })
 
   // Show author card when avatar is hovered
@@ -33,55 +34,4 @@ require([], () => {
       })
     })
 
-  /**
-   * Nav/Title replacement
-   *
-   * Displays the post title in place of the nav bar when scrolling past the title
-   *
-   * Usage:
-   * ```
-   * stickyTitle({
-   *     navSelector: '.site-nav-main',
-   *     titleSelector: '.post-full-title',
-   *     activeClass: 'nav-post-title-active'
-   * });
-   * ```
-   */
-  function stickyNavTitle(options) {
-    const nav = document.querySelector(options.navSelector)
-    const title = document.querySelector(options.titleSelector)
-
-    var lastScrollY = window.scrollY
-    var ticking = false
-
-    function onScroll() {
-      lastScrollY = window.scrollY
-      requestTick()
-    }
-
-    function requestTick() {
-      if (!ticking) {
-        requestAnimationFrame(update)
-      }
-      ticking = true
-    }
-
-    function update() {
-      const trigger = title.getBoundingClientRect().top + window.scrollY
-      const triggerOffset = title.offsetHeight + 35
-
-      // show/hide post title
-      if (lastScrollY >= trigger + triggerOffset) {
-        nav.classList.add(options.activeClass)
-      } else {
-        nav.classList.remove(options.activeClass)
-      }
-
-      ticking = false
-    }
-
-    window.addEventListener('scroll', onScroll, {passive: true})
-
-    update()
-  }
 })
