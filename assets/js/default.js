afb7e58add2754d4511ffbc3ad57b3ba8a49d747
Josh Perry <josh@6bit.com>
2020-03-22 10:49:12 -0600

    Fix katex lazy load code (say 3 times fast)

--- assets/js/default.js
+++ assets/js/default.js
@@ -5,7 +5,7 @@
 
 // Subresource Integrity hashes for CDN-loaded scripts
 const sri = {
-  katex: 'sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq'
+  katex: 'sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz'
 }
 
 function getScriptPath() {
@@ -20,7 +20,7 @@ require.config({
 
   paths: {
     app: '../app',
-    katex: 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js',
+    katex: 'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min',
   },
 
   shim: {
